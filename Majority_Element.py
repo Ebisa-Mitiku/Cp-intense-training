9068dedc6ad6abf7bc6383b7ebfad117df3ca1ff
Ebisa-Mitiku <ebisamitiku203@gmail.com>
2026-03-06 13:53:09 +0300

    Create Majority_Element.py

---
+++ Majority_Element.py
@@ -0,0 +1,13 @@
+class Solution:
+    def majorityElement(self, nums: List[int]) -> int:
+        n=len(nums)
+        count={}
+        for i in nums:
+            if i in count:
+                count[i]+=1
+            else:
+                count[i]=1
+        for key in count:
+            if(count[key]>n//2):
+                return(key)
+                break
